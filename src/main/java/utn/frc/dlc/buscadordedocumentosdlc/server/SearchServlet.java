fa76a07f13dc334d529699e69ff2212edb3a831e
Gonzalo Saad [glb] <saad.gonzalo.ale@gmail.com>
2018-04-19 15:52:43 -0500

    Primer commit con la estructura del proyecto

---
+++ src/main/java/utn/frc/dlc/buscadordedocumentosdlc/server/SearchServlet.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utn.frc.dlc.buscadordedocumentosdlc.server;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import utn.frc.dlc.buscadordedocumentosdlc.core.searchengine.SearchEngineController;
+
+/**
+ *
+ * @author gonzalo.saad
+ */
+@WebServlet(urlPatterns = "/search")
+public class SearchServlet extends HttpServlet {
+    
+    private static SearchEngine searchEngine = new SearchEngineController();
+    
+    
+    @Override
+    protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+        httpServletRequest.getRequestDispatcher("/WEB-INF/views/searchHome.jsp").forward(httpServletRequest,httpServletResponse);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+    }
+    
+}
---
+++ src/main/webapp/WEB-INF/views/searchHome.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Buscador de Documentos</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+    <body>
+        <h1>Hello World from JSP!</h1>
+    </body>
+</html>
